87418b00d4ef91fb482e18313f042e769a77ef60
Nel <nel@flexio.fr>
2018-05-29 09:17:45 +0200

    mongo tests : upgraded mongo version to 3.6.5 added container prefix and container cleanup in pipeline

--- flexio-mongo-tests/src/main/java/io/flexio/services/tests/mongo/MongoTest.java
+++ flexio-mongo-tests/src/main/java/io/flexio/services/tests/mongo/MongoTest.java
@@ -6,11 +6,15 @@ import java.util.function.Supplier;
 
 public class MongoTest {
 
-    static public final String MONGO = "mongo-ut";
+    static public final String MONGO = System.getProperty("ut.container.prefix", "") + "mongo-ut";
+    static public final String MONGO_IMAGE = "mongo:" + System.getProperty("ut.mongo.version", "3.6.5");
 
     static public DockerResource.ContainerInitialStatus docker() {
         return DockerResource.client()
-                .with(MONGO, container -> container.image("mongo:3.4.10"));
+                .with(MONGO, container -> {
+                    System.out.println("###### MONGO IMAGE " + MONGO_IMAGE);
+                    return container.image(MONGO_IMAGE);
+                });
     }
 
     static public MongoResource mongo(Supplier<DockerResource> dockerSupplier) {
