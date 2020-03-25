e31291002cb8c8fc317aedbce475fcde65d28785
nelt <nel@flexio.fr>
2020-03-25 06:57:29 +0100

    upgraded to snapshot

--- flexio-mongo-tests/src/main/java/io/flexio/services/tests/mongo/dump/DatabaseRestorer.java
+++ flexio-mongo-tests/src/main/java/io/flexio/services/tests/mongo/dump/DatabaseRestorer.java
@@ -1,10 +1,13 @@
 package io.flexio.services.tests.mongo.dump;
 
 import com.mongodb.MongoClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class DatabaseRestorer {
+    static private final Logger log = LoggerFactory.getLogger(DatabaseRestorer.class);
 
     private final MongoClient client;
     private final DumpReader dumpReader;
@@ -17,9 +20,10 @@ public class DatabaseRestorer {
     public void restoreTo(String targetDb) throws IOException {
         this.client.getDatabase(targetDb).drop();
 
+        log.debug("RESTORE TO START");
         for (String collection : this.dumpReader.collections()) {
             new CollectionRestorer(this.client, this.dumpReader.collectionData(collection)).restoreTo(targetDb, collection);
         }
-
+        log.debug("RESTORE TO END");
     }
 }
