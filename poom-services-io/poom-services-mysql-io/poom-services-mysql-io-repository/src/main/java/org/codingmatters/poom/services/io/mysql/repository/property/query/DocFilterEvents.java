8e63969834baac2d8d2a0c5e4351d83f9dcca9b7
nelt <nel@flexio.fr>
2020-12-03 22:59:09 +0100

    [mysql repo] property query - almost complete...
    ref #62

--- poom-services-io/poom-services-mysql-io/poom-services-mysql-io-repository/src/main/java/org/codingmatters/poom/services/io/mysql/repository/property/query/DocFilterEvents.java
+++ poom-services-io/poom-services-mysql-io/poom-services-mysql-io-repository/src/main/java/org/codingmatters/poom/services/io/mysql/repository/property/query/DocFilterEvents.java
@@ -4,17 +4,136 @@ import org.codingmatters.poom.services.domain.property.query.FilterEvents;
 import org.codingmatters.poom.services.domain.property.query.events.FilterEventError;
 import org.codingmatters.poom.services.io.mysql.repository.table.TableModel;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class DocFilterEvents implements FilterEvents {
-    StringBuilder clauseBuilder = new StringBuilder();
+    private final StringBuilder clauseBuilder = new StringBuilder();
+    private final List<TableModel.ParamSetter> paramsSetters = new LinkedList<>();
 
     @Override
     public Object isEquals(String left, Object right) throws FilterEventError {
-        this.clauseBuilder.append(String.format("JSON_EXTRACT(doc, \"$.%s\") = ?", left));
+        this.appendSimplePredicate(left, "=", right);
         return null;
+    }
+
+    @Override
+    public Object isEqualsProperty(String left, String right) throws FilterEventError {
+        this.appendPropertySimplePredicate(left, "=", right);
+        return null;
+    }
+
+    @Override
+    public Object isNotEquals(String left, Object right) throws FilterEventError {
+        this.appendSimplePredicate(left, "!=", right);
+        return null;
+    }
+
+    @Override
+    public Object isNotEqualsProperty(String left, String right) throws FilterEventError {
+        this.appendPropertySimplePredicate(left, "!=", right);
+        return null;
+    }
+
+    @Override
+    public Object isNull(String property) throws FilterEventError {
+        this.appendNullPredicate(property, "=");
+        return null;
+    }
+
+    @Override
+    public Object isNotNull(String property) throws FilterEventError {
+        this.appendNullPredicate(property, "!=");
+        return null;
+    }
+
+    @Override
+    public Object graterThan(String left, Object right) throws FilterEventError {
+        this.appendSimplePredicate(left, ">", right);
+        return null;
+    }
+
+    @Override
+    public Object graterThanProperty(String left, String right) throws FilterEventError {
+        this.appendPropertySimplePredicate(left, ">", right);
+        return null;
+    }
+
+    @Override
+    public Object graterThanOrEquals(String left, Object right) throws FilterEventError {
+        this.appendSimplePredicate(left, ">=", right);
+        return null;
+    }
+
+    @Override
+    public Object graterThanOrEqualsProperty(String left, String right) throws FilterEventError {
+        this.appendPropertySimplePredicate(left, ">=", right);
+        return null;
+    }
+
+    @Override
+    public Object lowerThan(String left, Object right) throws FilterEventError {
+        this.appendSimplePredicate(left, "<", right);
+        return null;
+    }
+
+    @Override
+    public Object lowerThanProperty(String left, String right) throws FilterEventError {
+        this.appendPropertySimplePredicate(left, "<", right);
+        return null;
+    }
+
+    @Override
+    public Object lowerThanOrEquals(String left, Object right) throws FilterEventError {
+        this.appendSimplePredicate(left, "<=", right);
+        return null;
+    }
+
+    @Override
+    public Object lowerThanOrEqualsProperty(String left, String right) throws FilterEventError {
+        this.appendPropertySimplePredicate(left, "<=", right);
+        return null;
+    }
+
+    @Override
+    public Object startsWith(String left, Object right) throws FilterEventError {
+        this.appendSimplePredicate(left, "LIKE", right + "%");
+        return null;
+    }
+
+    @Override
+    public Object endsWith(String left, Object right) throws FilterEventError {
+        this.appendSimplePredicate(left, "LIKE", "%" + right);
+        return null;
+    }
+
+    @Override
+    public Object contains(String left, Object right) throws FilterEventError {
+        this.appendSimplePredicate(left, "LIKE", "%" + right + "%");
+        return null;
+    }
+
+    private void appendSimplePredicate(String propertyPath, String operator, Object value) {
+        this.clauseBuilder.append(String.format("JSON_VALUE(JSON_EXTRACT(doc, \"$.%s\"), \"$\") %s ?", propertyPath, operator));
+        this.paramsSetters.add((statement, index) -> statement.setObject(index, value));
+    }
+
+    private void appendNullPredicate(String propertyPath, String operator) {
+        this.clauseBuilder.append(String.format("JSON_VALUE(JSON_EXTRACT(doc, \"$.%s\"), \"$\") %s 'null'", propertyPath, operator));
+    }
+
+    private void appendPropertySimplePredicate(String leftProperty, String operator, String rightProperty) {
+        this.clauseBuilder.append(String.format(
+                "JSON_VALUE(JSON_EXTRACT(doc, \"$.%s\"), \"$\") %s JSON_VALUE(JSON_EXTRACT(doc, \"$.%s\"), \"$\")",
+                leftProperty, operator, rightProperty));
     }
 
     public TableModel.Clause clause() {
         String clause = this.clauseBuilder.toString();
-        return new TableModel.Clause(clause != null ? clause : null);
+        System.out.println(clause);
+        return new TableModel.Clause(
+                clause != null ? clause : null,
+                this.paramsSetters.isEmpty() ? null : this.paramsSetters.toArray(new TableModel.ParamSetter[0])
+        );
     }
 }
---
+++ poom-services-io/poom-services-mysql-io/poom-services-mysql-io-repository/src/test/java/org/codingmatters/poom/services/io/mysql/repository/property/query/MySQLJsonRepositoryWithPropertyQueryTest.java
@@ -0,0 +1,288 @@
+package org.codingmatters.poom.services.io.mysql.repository.property.query;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import io.flexio.docker.DockerResource;
+import org.codingmatters.generated.ComplexValue;
+import org.codingmatters.generated.complexvalue.Nested;
+import org.codingmatters.generated.json.ComplexValueReader;
+import org.codingmatters.generated.json.ComplexValueWriter;
+import org.codingmatters.poom.services.domain.property.query.PropertyQuery;
+import org.codingmatters.poom.services.io.mysql.repository.MariaDBResource;
+import org.codingmatters.poom.services.io.mysql.repository.MySQLJsonRepository;
+import org.codingmatters.poom.servives.domain.entities.Entity;
+import org.codingmatters.poom.servives.domain.entities.PagedEntityList;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.Arrays;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class MySQLJsonRepositoryWithPropertyQueryTest {
+
+    @ClassRule
+    static public DockerResource docker = DockerResource.client();
+
+    @Rule
+    public MariaDBResource mariaDBResource = new MariaDBResource(docker);
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    private final JsonFactory jsonFactory = new JsonFactory();
+    private MySQLJsonRepository<ComplexValue, PropertyQuery> repository;
+
+    @Before
+    public void setUp() throws Exception {
+        this.repository = new MySQLJsonRepository<ComplexValue, PropertyQuery>(
+                this.mariaDBResource.ds(),
+                "repository",
+                this.jsonFactory,
+                (generator, value) -> new ComplexValueWriter().write(generator, value),
+                parser -> new ComplexValueReader().read(parser),
+                new PropertyQueryToDocQueryParser()
+        );
+        for (int i = 0; i < 100; i++) {
+            this.repository.create(ComplexValue.builder()
+                    .stringProp("%03d", i)
+                    .nested(Nested.builder()
+                            .nestedProp("%03d", 100 - i)
+                            .build())
+                    .build());
+        }
+    }
+
+    @Test
+    public void whenNoFilter_andNoOrderBy__thenAllValuesReturned() throws Exception {
+        PagedEntityList<ComplexValue> actual = this.repository.search(PropertyQuery.builder().build(), 0, 1000);
+
+        assertThat(actual.total(), is(this.repository.all(0, 0).total()));
+    }
+
+    @Test
+    public void givenFilterOnStringProperty__whenIsEqual__thenSelectedValueReturned() throws Exception {
+        PagedEntityList<ComplexValue> actual = this.repository.search(PropertyQuery.builder()
+                .filter("stringProp == '006'")
+                .build(), 0, 1000);
+
+        assertThat(actual.total(), is(1L));
+        assertThat(actual.get(0).value().stringProp(), is("006"));
+    }
+
+    @Test
+    public void givenFilterOnStringNestedProperty__whenIsEqual__thenSelectedValueReturned() throws Exception {
+        PagedEntityList<ComplexValue> actual = this.repository.search(PropertyQuery.builder()
+                .filter("nested.nestedProp == '006'")
+                .build(), 0, 1000);
+
+        assertThat(actual.total(), is(1L));
+        assertThat(actual.get(0).value().nested().nestedProp(), is("006"));
+    }
+
+    @Test
+    public void givenFilterOnStringProperty__whenIsEqualProperty__thenSelectedValueReturned() throws Exception {
+        PagedEntityList<ComplexValue> actual = this.repository.search(PropertyQuery.builder()
+                .filter("stringProp == nested.nestedProp")
+                .build(), 0, 1000);
+
+        assertThat(actual.total(), is(1L));
+        assertThat(actual.get(0).value().stringProp(), is("050"));
+        assertThat(actual.get(0).value().nested().nestedProp(), is("050"));
+    }
+
+    @Test
+    public void givenFilterOnStringProperty__whenNotIsEqual__thenSelectedValueReturned() throws Exception {
+        PagedEntityList<ComplexValue> actual = this.repository.search(PropertyQuery.builder()
+                .filter("stringProp != '006'")
+                .build(), 0, 1000);
+
+        assertThat(actual.total(), is(99L));
+        assertThat(
+                actual.valueList().stream().map(complexValue -> complexValue.stringProp()).toArray(),
+                not(hasItemInArray("006"))
+        );
+    }
+
+    @Test
+    public void givenFilterOnStringProperty__whenNotIsEqualProperty__thenSelectedValueReturned() throws Exception {
+        PagedEntityList<ComplexValue> actual = this.repository.search(PropertyQuery.builder()
+                .filter("stringProp != nested.nestedProp")
+                .build(), 0, 1000);
+
+        assertThat(actual.total(), is(99L));
+        assertThat(
+                actual.valueList().stream().map(complexValue -> complexValue.stringProp()).toArray(),
+                not(hasItemInArray("050"))
+        );
+    }
+
+    @Test
+    public void givenFilterOnStringProperty__whenIsNull__thenSelectedValueReturned() throws Exception {
+        Entity<ComplexValue> entity = this.repository.create(ComplexValue.builder().stringProp(null).build());
+
+        PagedEntityList<ComplexValue> actual = this.repository.search(PropertyQuery.builder()
+                .filter("stringProp == null")
+                .build(), 0, 1000);
+
+        assertThat(actual.total(), is(1L));
+        assertThat(actual.get(0).id(), is(entity.id()));
+    }
+
+    @Test
+    public void givenFilterOnStringProperty__whenIsNotNull__thenSelectedValueReturned() throws Exception {
+        Entity<ComplexValue> entity = this.repository.create(ComplexValue.builder().stringProp(null).build());
+
+        PagedEntityList<ComplexValue> actual = this.repository.search(PropertyQuery.builder()
+                .filter("stringProp != null")
+                .build(), 0, 1000);
+
+        assertThat(actual.total(), is(100L));
+        assertThat(
+                actual.stream().map(complexValueEntity -> complexValueEntity.id()).toArray(),
+                not(hasItemInArray(entity.id()))
+        );
+    }
+
+    @Test
+    public void givenFilterOnStringProperty__whenGraterThan__thenSelectedValueReturned() throws Exception {
+        PagedEntityList<ComplexValue> actual = this.repository.search(PropertyQuery.builder()
+                .filter("stringProp > '097'")
+                .build(), 0, 1000);
+
+        assertThat(actual.total(), is(2L));
+        assertThat(
+                actual.valueList().stream().map(complexValue -> complexValue.stringProp()).toArray(),
+                is(arrayContainingInAnyOrder("098", "099"))
+        );
+    }
+
+    @Test
+    public void givenFilterOnStringProperty__whenGraterThanProperty__thenSelectedValueReturned() throws Exception {
+        PagedEntityList<ComplexValue> actual = this.repository.search(PropertyQuery.builder()
+                .filter("stringProp > nested.nestedProp")
+                .build(), 0, 1000);
+
+        assertThat(actual.total(), is(49L));
+        for (Entity<ComplexValue> entity : actual) {
+            assertThat(entity.value().toString(), entity.value().stringProp(), is(greaterThan(entity.value().nested().nestedProp())));
+        }
+    }
+
+    @Test
+    public void givenFilterOnStringProperty__whenGraterThanOrEquals__thenSelectedValueReturned() throws Exception {
+        PagedEntityList<ComplexValue> actual = this.repository.search(PropertyQuery.builder()
+                .filter("stringProp >= '097'")
+                .build(), 0, 1000);
+
+        assertThat(actual.total(), is(3L));
+        assertThat(
+                actual.valueList().stream().map(complexValue -> complexValue.stringProp()).toArray(),
+                is(arrayContainingInAnyOrder("097", "098", "099"))
+        );
+    }
+
+    @Test
+    public void givenFilterOnStringProperty__whenGraterThanOrEqualProperty__thenSelectedValueReturned() throws Exception {
+        PagedEntityList<ComplexValue> actual = this.repository.search(PropertyQuery.builder()
+                .filter("stringProp >= nested.nestedProp")
+                .build(), 0, 1000);
+
+        assertThat(actual.total(), is(50L));
+        for (Entity<ComplexValue> entity : actual) {
+            assertThat(entity.value().toString(), entity.value().stringProp(), is(greaterThanOrEqualTo(entity.value().nested().nestedProp())));
+        }
+    }
+
+    @Test
+    public void givenFilterOnStringProperty__whenLowerThan__thenSelectedValueReturned() throws Exception {
+        PagedEntityList<ComplexValue> actual = this.repository.search(PropertyQuery.builder()
+                .filter("stringProp < '002'")
+                .build(), 0, 1000);
+
+        assertThat(actual.total(), is(2L));
+        assertThat(
+                actual.valueList().stream().map(complexValue -> complexValue.stringProp()).toArray(),
+                is(arrayContainingInAnyOrder("000", "001"))
+        );
+    }
+
+    @Test
+    public void givenFilterOnStringProperty__whenLowerThanProperty__thenSelectedValueReturned() throws Exception {
+        PagedEntityList<ComplexValue> actual = this.repository.search(PropertyQuery.builder()
+                .filter("stringProp < nested.nestedProp")
+                .build(), 0, 1000);
+
+        assertThat(actual.total(), is(50L));
+        for (Entity<ComplexValue> entity : actual) {
+            assertThat(entity.value().toString(), entity.value().stringProp(), is(lessThan(entity.value().nested().nestedProp())));
+        }
+    }
+
+    @Test
+    public void givenFilterOnStringProperty__whenLowerThanOrEquals__thenSelectedValueReturned() throws Exception {
+        PagedEntityList<ComplexValue> actual = this.repository.search(PropertyQuery.builder()
+                .filter("stringProp <= '002'")
+                .build(), 0, 1000);
+
+        assertThat(actual.total(), is(3L));
+        assertThat(
+                actual.valueList().stream().map(complexValue -> complexValue.stringProp()).toArray(),
+                is(arrayContainingInAnyOrder("000", "001", "002"))
+        );
+    }
+
+    @Test
+    public void givenFilterOnStringProperty__whenLowerThanOrEqualProperty__thenSelectedValueReturned() throws Exception {
+        PagedEntityList<ComplexValue> actual = this.repository.search(PropertyQuery.builder()
+                .filter("stringProp <= nested.nestedProp")
+                .build(), 0, 1000);
+
+        assertThat(actual.total(), is(51L));
+        for (Entity<ComplexValue> entity : actual) {
+            assertThat(entity.value().toString(), entity.value().stringProp(), is(lessThanOrEqualTo(entity.value().nested().nestedProp())));
+        }
+    }
+
+    @Test
+    public void givenFilterOnStringProperty__whenStartsWith__thenSelectedValueReturned() throws Exception {
+        PagedEntityList<ComplexValue> actual = this.repository.search(PropertyQuery.builder()
+                .filter("stringProp starts with '01'")
+                .build(), 0, 1000);
+
+        assertThat(actual.total(), is(10L));
+        assertThat(
+                actual.valueList().stream().map(complexValue -> complexValue.stringProp()).toArray(),
+                is(arrayContainingInAnyOrder("010", "011", "012", "013", "014", "015", "016", "017", "018", "019"))
+        );
+    }
+
+    @Test
+    public void givenFilterOnStringProperty__whenEndsWith__thenSelectedValueReturned() throws Exception {
+        PagedEntityList<ComplexValue> actual = this.repository.search(PropertyQuery.builder()
+                .filter("stringProp ends with '8'")
+                .build(), 0, 1000);
+
+        assertThat(actual.total(), is(10L));
+        assertThat(
+                actual.valueList().stream().map(complexValue -> complexValue.stringProp()).toArray(),
+                is(arrayContainingInAnyOrder("008", "018", "028", "038", "048", "058", "068", "078", "088", "098"))
+        );
+    }
+
+    @Test
+    public void givenFilterOnStringProperty__whenContains__thenSelectedValueReturned() throws Exception {
+        PagedEntityList<ComplexValue> actual = this.repository.search(PropertyQuery.builder()
+                .filter("stringProp contains '08'")
+                .build(), 0, 1000);
+
+        assertThat(actual.total(), is(11L));
+        assertThat(
+                actual.valueList().stream().map(complexValue -> complexValue.stringProp()).toArray(),
+                is(arrayContainingInAnyOrder("008", "080", "081", "082", "083", "084", "085", "086", "087", "088", "089"))
+        );
+    }
+}
